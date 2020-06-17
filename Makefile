64e5d6e6b2c13a4db709f65485256b8bb77b6853
nicks96432 <nicks96432@gmail.com>
2020-06-17 21:15:12 +0800

    今天先這樣

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ all:
 	g++ main.cpp -o main -O2 -std=c++11
 clean:
 	rm -rf main
+run:
+	./main
+debug_run:
+	/usr/bin/time -v ./main
--- main.cpp
+++ main.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-// #include "mails.hpp"
+#include <unordered_set>
+#include "mails.hpp"
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	ifstream file("../final/test_data/mail1");
-	string tmp;
-	for (int _ = 0; _ < 7; _++)
+	// ios_base::sync_with_stdio(false);
+	// cin.tie(NULL);
+	const unordered_set<string> commands =
+		{
+			"longest",
+			"add",
+			"query",
+			"remove"
+
+		};
+	Mails mails;
+	string command, path;
+	int id;
+	while (cin >> command)
 	{
-		file >> tmp;
-		cout << tmp << '\n';
+		auto it = commands.find(command);
+		if (it == commands.cend())
+			throw out_of_range(command + ": Invalid command");
+		switch (it->front())
+		{
+		case 'a':
+			cin >> path;
+			mails.add(path);
+			break;
+		case 'r':
+			cin >> id;
+			mails.remove(id);
+			break;
+		case 'q':
+			cin >> path;
+			mails.query(path);
+			break;
+		default: // longest
+			mails.longest();
+			break;
+		}
 	}
-	int n;
-	file >> n;
-	cout << n << '\n';
-	file >> n;
-	cout << n << '\n';
-	file >> tmp;
-	cout << tmp << '\n';
-	file.close();
 	return 0;
 }
