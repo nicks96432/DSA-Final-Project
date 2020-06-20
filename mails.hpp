b97d166f44445972e81250b61210206301c205f6
nicks96432 <nicks96432@gmail.com>
2020-06-20 19:39:25 +0800

    定期存檔

--- mails.hpp
+++ mails.hpp
@@ -2,6 +2,8 @@
 
 #define MAILS_HPP
 
+#pragma once
+
 #include <set>
 #include <map>
 #include <ctime>
@@ -47,23 +49,6 @@ static const std::string number_to_month[12] =
 		std::string("December")
 
 };
-// 清空stringstream
-static inline void reset(std::stringstream &ss)
-{
-	ss.clear();
-	ss.str(std::string());
-}
-static std::istream &myGetline(std::istream &in, std::string &str)
-{
-	str.clear();
-	char c;
-	while ((c = in.get()) <= 32 && !in.fail());
-	if (c != 4)
-		str.push_back(c);
-	while ((c = in.get()) != '\n' && !in.fail())
-		str.push_back(c);
-	return in;
-}
 /* 信件格式範例
  * From: Tim
  * Date: 19 May 2011 at 16:50
@@ -74,13 +59,12 @@ static std::istream &myGetline(std::istream &in, std::string &str)
  * Minas-Rio is an iron ore mining project in Brazil.[1] It is one of the world's largest mining projects, and is initially expected to export 26.5 million tonnes of iron ore in 2013, through a 525km slurry pipeline to a port at Au;[2] production potential is 53 Mtpa or higher.[3] The project was bought by Anglo American PLC, which is facing high costs.[4][5] The mine has certified reserves of 4.6 billion tonnes of itabirite.[6] There have been delays in starting the project, but in December 2010, Anglo American obtained a key license needed from Brazilian government before mining could start.[7][8] 
  * I read the paragraph on http://wikipedia.org
  */
-inline bool isAlnum(const char &c)
+inline void string_lower(std::string &str)
 {
-	if ((c >= 'a' && c <= 'z') ||
-		(c >= 'A' && c <= 'Z') ||
-		(c >= '0' && c <= '9'))
-		return true;
-	return false;
+	for (auto &it : str)
+		if (std::isupper(it))
+			it = std::tolower(it);
+	return;
 }
 class Mail
 {
@@ -93,64 +77,103 @@ public:
 	Mail(const std::string &p)
 	{
 		std::ifstream file(p);
-		file.sync_with_stdio(false);
-		file.tie(NULL);
 		if (file.fail())
 			throw std::runtime_error(p + ": No such file or directory.");
+		file.sync_with_stdio(false);
+		file.tie(NULL);
 		path = p;
 		isopen = true;
 		std::string tmp(50, '\0');
-		char _;
 		// From from
 		std::getline(file, tmp);
-		from = std::string(tmp.begin() + 6, tmp.end());
+		from = &tmp.at(6);
+		string_lower(from);
 		// Date day
 		std::getline(file, tmp);
-		char tmp2[12];
-		sscanf(tmp.c_str(), "Date: %d %s %d at %d:%d", &datetime.day,
-			   tmp2, &datetime.year, &datetime.hour, &datetime.minute);
+		auto it_str = tmp.begin() + 6;
+		for (; it_str < tmp.end(); ++it_str)
+		{
+			if (*it_str == ' ')
+			{
+				++it_str;
+				break;
+			}
+			datetime.day = datetime.day * 10 + (*it_str - '0');
+		}
+		auto month_start = it_str;
+		std::string tmp2(12, '\0');
+		for (; it_str < tmp.end(); ++it_str)
+		{
+			if (*it_str == ' ')
+			{
+				*it_str = '\0';
+				++it_str;
+				break;
+			}
+		}
+		tmp2 = &(*month_start);
 		auto it = month_to_number.find(tmp2);
 		if (it == month_to_number.cend())
-			throw std::out_of_range("invalid month: " + std::string(tmp2));
+			throw std::out_of_range("invalid month: " + tmp2);
 		datetime.month = it->second;
+		for (; it_str < tmp.end(); ++it_str)
+		{
+			if (*it_str == ' ')
+			{
+				++it_str;
+				break;
+			}
+			datetime.year = datetime.year * 10 + (*it_str - '0');
+		}
+		it_str += 3;
+		datetime.hour = (*it_str - '0') * 10 + (*(it_str + 1) - '0');
+		datetime.minute = (*(it_str + 3) - '0') * 10 + (*(it_str + 4) - '0');
 		// Message-ID
+		datetime.to_number();
 		std::getline(file, tmp);
-		sscanf(tmp.c_str(), "Message-ID: %d", &MessageID);
+		MessageID = std::stoi(std::string(tmp.begin() + 12, tmp.end()));
 		// Subject
 		std::getline(file, tmp);
-		for (auto iter = tmp.begin() + 8; iter <= tmp.end(); ++iter)
+		for (auto iter = tmp.begin() + 8; iter < tmp.end(); ++iter)
 		{
-			while (iter <= tmp.end() && !isAlnum(*iter))
+			while (iter < tmp.end() && !std::isalnum(*iter))
 				++iter;
 			auto start = iter;
-			while (iter <= tmp.end() && isAlnum(*iter))
+			while (iter < tmp.end() && std::isalnum(*iter))
+			{
+				if (std::isupper(*iter))
+					*iter = std::tolower(*iter);
 				++iter;
+			}
 			subject.insert(std::string(start, iter));
 		}
 		// To
 		std::getline(file, tmp);
 		to = std::string(tmp.begin() + 4, tmp.end());
+		string_lower(to);
 		// content:
 		std::getline(file, tmp);
 		// real content
 		charcount = 0;
 		while (std::getline(file, tmp))
 		{
-			for (auto iter = tmp.begin(); iter <= tmp.end(); ++iter)
+			for (auto iter = tmp.begin(); iter < tmp.end(); ++iter)
 			{
-				while (iter != tmp.end() && !isAlnum(*iter))
+				while (iter < tmp.end() && !std::isalnum(*iter))
 					++iter;
 				auto start = iter;
-				while (iter != tmp.end() && isAlnum(*iter))
+				while (iter < tmp.end() && std::isalnum(*iter))
+				{
+					if (isupper(*iter))
+						*iter = std::tolower(*iter);
 					++iter;
+				}
 				std::string ins(start, iter);
 				charcount += ins.length();
 				content.insert(ins);
 			}
 		}
 		file.close();
-		// std::cout << *this;
-		// exit(-1);
 	}
 	inline int ID() const
 	{
@@ -198,7 +221,22 @@ public:
 private:
 	struct Datetime
 	{
+		Datetime()
+		{
+			year = 0;
+			month = 0;
+			day = 0;
+			hour = 0;
+			minute = 0;
+			time_number = 0;
+		}
+		inline void to_number()
+		{
+			time_number = year * 100000000LL + month * 1000000LL +
+						  day * 10000LL + hour * 100LL + minute * 1LL;
+		}
 		int year, month, day, hour, minute;
+		long long time_number;
 	};
 	Datetime datetime;
 	std::string from, to;
@@ -214,6 +252,9 @@ public:
 	Mails()
 	{
 		count = 0;
+		maildata_ID.reserve(10000);
+		maildata_path.reserve(10000);
+		maildata_heap.reserve(10000);
 	}
 	void add(const std::string &path)
 	{
@@ -228,9 +269,9 @@ public:
 			else
 			{
 				it->second.openFile();
-				maildata_heap.push_back(it);
+				maildata_heap.push_back(&it->second);
 				std::push_heap(maildata_heap.begin(), maildata_heap.begin() + ++count,
-							   iterator_compare);
+							   compare);
 				std::cout << count << '\n';
 				return;
 			}
@@ -239,25 +280,24 @@ public:
 		{
 			Mail mail(path);
 			maildata_path[path] = mail;
-			auto it = maildata_path.find(path);
-			maildata_ID[mail.ID()] = it;
-			maildata_heap.push_back(it);
+			maildata_ID[mail.ID()] = &maildata_path[path];
+			maildata_heap.push_back(&maildata_path[path]);
 			std::push_heap(maildata_heap.begin(), maildata_heap.begin() + ++count,
-						   iterator_compare);
-			// std::cout << count << '\n';
+						   compare);
+			std::cout << count << '\n';
 		}
 	}
 	void remove(const int &id)
 	{
 		auto it = maildata_ID.find(id);
-		if (it == maildata_ID.end() || !it->second->second.isOpen())
+		if (it == maildata_ID.end() || !it->second->isOpen())
 		{
 			std::cout << "-\n";
 			return;
 		}
-		it->second->second.closeFile();
+		it->second->closeFile();
 		std::pop_heap(maildata_heap.begin(), maildata_heap.begin() + count--,
-					  iterator_compare);
+					  compare);
 		maildata_heap.pop_back();
 		std::cout << count << '\n';
 	}
@@ -271,15 +311,16 @@ public:
 	 */
 	void query(const std::string &commands)
 	{
-		std::stringstream ss;
-		ss << commands;
+		std::cout << commands << '\n';
+		return;
 		std::string tmp;
-		while (ss >> tmp)
+		for(auto it = commands.begin();it!=commands.end();++it)
 		{
 			if (tmp.front() == '-')
 				switch (tmp.at(2))
 				{
 				case 'f':
+
 					break;
 				case 't':
 					break;
@@ -290,7 +331,7 @@ public:
 				}
 		}
 		std::cout << commands << '\n';
-		// std::cout << "-\n";
+		std::cout << "-\n";
 		return;
 	}
 	void longest(void) const
@@ -300,21 +341,20 @@ public:
 			std::cout << "-\n";
 			return;
 		}
-		std::cout << maildata_heap.front()->second.ID() << ' '
-				  << maildata_heap.front()->second.ContentLength() << '\n';
+		std::cout << maildata_heap.front()->ID() << ' '
+				  << maildata_heap.front()->ContentLength() << '\n';
 	}
 
 private:
-	typedef std::unordered_map<std::string, Mail>::iterator Mail_iterator;
 	int count;
 	std::unordered_map<std::string, Mail> maildata_path;
-	std::map<int, Mail_iterator> maildata_ID;
-	std::vector<Mail_iterator> maildata_heap;
-	std::function<bool(const Mail_iterator &, const Mail_iterator &)> iterator_compare =
-		[](const Mail_iterator &x, const Mail_iterator &y) -> bool {
-		if (x->second.ContentLength() == y->second.ContentLength())
-			return x->second.ID() < y->second.ID();
-		return x->second.ContentLength() < y->second.ContentLength();
+	std::unordered_map<int, Mail *> maildata_ID;
+	std::vector<const Mail *> maildata_heap;
+	std::function<bool(const Mail *&, const Mail *&)> compare =
+		[](const Mail *&x, const Mail *&y) -> bool {
+		if (x->ContentLength() == y->ContentLength())
+			return x->ID() < y->ID();
+		return x->ContentLength() < y->ContentLength();
 	};
 };
 
--- main.cpp
+++ main.cpp
@@ -18,36 +18,37 @@ int main(int argc, char const *argv[])
 	Mails mails;
 	string command, path;
 	int id;
-	for (int i = 1; i <= 10000; i++)
-	{
-		
-		string str = "../final/test_data/mail";
-		str.append(to_string(i));
-		mails.add(str);
-	}
-	// while (cin >> command)
+	// for (int i = 1; i <= 1; i++)
 	// {
-	// 	auto it = commands.find(command);
-	// 	if (it == commands.cend())
-	// 		throw out_of_range(command + ": Invalid command");
-	// 	switch (it->front())
-	// 	{
-	// 	case 'a':
-	// 		cin >> path;
-	// 		mails.add(path);
-	// 		break;
-	// 	case 'r':
-	// 		cin >> id;
-	// 		mails.remove(id);
-	// 		break;
-	// 	case 'q':
-	// 		getline(cin, path);
-	// 		mails.query(path);
-	// 		break;
-	// 	default: // longest
-	// 		mails.longest();
-	// 		break;
-	// 	}
+
+	// 	string str = "../final/test_data/mail";
+	// 	str.append(to_string(i));
+	// 	mails.add(str);
+	// 	cout << Mail(str);
 	// }
+	while (cin >> command)
+	{
+		auto it = commands.find(command);
+		if (it == commands.cend())
+			throw out_of_range(command + ": Invalid command");
+		switch (it->front())
+		{
+		case 'a':
+			cin >> path;
+			mails.add(path);
+			break;
+		case 'r':
+			cin >> id;
+			mails.remove(id);
+			break;
+		case 'q':
+			getline(cin, path);
+			mails.query(path);
+			break;
+		default: // longest
+			mails.longest();
+			break;
+		}
+	}
 	return 0;
 }
